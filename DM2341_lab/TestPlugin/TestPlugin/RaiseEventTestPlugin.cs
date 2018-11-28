a29e96a7d2a807b214c1738699638d4319c3fb8a
DESKTOP-VEJ40A3\Jadelays <jerrywong243@gmail.com>
2018-11-29 02:10:51 +0800

    completed on lab_work

--- DM2341_lab/TestPlugin/TestPlugin/RaiseEventTestPlugin.cs
+++ DM2341_lab/TestPlugin/TestPlugin/RaiseEventTestPlugin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,19 +13,12 @@ namespace TestPlugin
 {
     public class RaiseEventTestPlugin : PluginBase
     {
-        string connStr;
+        readonly string connStr = "server=localhost;user=root;database=photon;port=3306;password=qwerty";
         MySqlConnection conn;
+        string recvMessage;
 
-        public string ServerString
-        {
-            get;
-            private set;
-        }
-        public int CallsCount
-        {
-            get;
-            private set;
-        }
+        public string ServerString { get; private set; }
+        public int CallsCount { get; private set; }
 
         public RaiseEventTestPlugin()
         {
@@ -57,10 +50,11 @@ namespace TestPlugin
                 return;
             }
 
+            // to insert into database
             if (1 == info.Request.EvCode)
             {
                 string playerName = Encoding.Default.GetString((byte[])info.Request.Data);
-                string sql = "INSERT INTO users (name, date_created) VALUES ('" + playerName + "', now())";
+                string sql = "INSERT INTO user (name, date_created) VALUES ('" + playerName + "', now())";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 cmd.ExecuteNonQuery();
 
@@ -68,18 +62,47 @@ namespace TestPlugin
                 int cnt = CallsCount;
                 string ReturnMessage = info.Nickname + " clicked the button. Now the count is " + cnt.ToString();
 
+                // returns the message to the client
                 PluginHost.BroadcastEvent(target: ReciverGroup.All,
                                           senderActor: 0,
                                           targetGroup: 0,
                                           data: new Dictionary<byte, object>() { { 245, ReturnMessage } },
-                                          evCode: info.Request.EvCode, cacheOp: 0);
+                                          evCode: info.Request.EvCode, 
+                                          cacheOp: 0);
             }
+            // testing with viking client
+            else if (2 == info.Request.EvCode)
+            {
+                recvMessage = Encoding.Default.GetString((byte[])info.Request.Data);
+
+                string playerName = GetStringDataFromMessage("PlayerName");
+                string playerPassword = GetStringDataFromMessage("Password");
+
+                string sql = "INSERT INTO viking (name, password, date_created) VALUES ('" + playerName + "', '" + playerPassword + "', now())";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        string GetStringDataFromMessage(string returnData)
+        {
+            string temp = returnData + "=";
+            int pFrom = recvMessage.IndexOf(temp) + temp.Length;
+            int pTo = recvMessage.LastIndexOf(",");
+
+            // there's no comma at the end
+            if (pTo - pFrom < 0)
+            {
+                pFrom = recvMessage.LastIndexOf(temp) + temp.Length;
+                return recvMessage.Substring(pFrom);
+            }
+
+            return recvMessage.Substring(pFrom, pTo - pFrom);
         }
 
         public void ConnectToMySQL()
         {
             // Connect to MySQL
-            connStr = "server=localhost;user=root;database=photon;port=3306;password=qwerty";
             conn = new MySqlConnection(connStr);
             try
             {
