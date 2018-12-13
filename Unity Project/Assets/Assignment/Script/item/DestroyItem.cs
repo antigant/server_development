d403fbb14597237d64f4595fc63c6bd986fb5759
DESKTOP-VEJ40A3\Jadelays <jerrywong243@gmail.com>
2018-12-14 05:40:38 +0800

    Item officially donegit add -A so damn happy

---
+++ Unity Project/Assets/Assignment/Script/item/DestroyItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItem : Photon.MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        Vector3 startPos = Player.GetInstance().GetPosition();
+        startPos.y -= 0.6f;
+        startPos.z += 0.6f;
+        transform.position = startPos;
+
+        GetComponent<Rigidbody>().AddForce(Player.GetInstance().GetForward() * 200.0f, ForceMode.Force);
+    }
+
+    public void DestroyThisItem()
+    {
+        Destroy(gameObject);
+    }
+}
